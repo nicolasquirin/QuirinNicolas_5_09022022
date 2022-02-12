635f46cb58f796494a329894dfebce2b9f95f9ef
nicolasquirin <nicolasquirin22@gmail.com>
2022-02-12 20:35:55 +0100

    update-product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,14 +1,60 @@
-// Récupération de la chaine de requête Url (methode window.location).
+// Récupération de la chaine de requête Url de la page actuel (methode window.location).
 
 const queryString_url_id = window.location.search;
 
-// Récupération de l'Id du produit selectionner.
+// Récupération de l'Id du produit selectionner avec la méthode UrlSearchParams.
 
 const urlSearcheParams = new URLSearchParams(queryString_url_id);
 const newUrlSearchParams = urlSearcheParams.get("id");
 console.log(newUrlSearchParams);
 
-// Utilisation de l'Id des produits.
+// Inplantation des elements dans le DOM.
+
+let img = document.createElement("img");
+img.src = "/front/kanap01.jpeg";
+let div = document.getElementsByClassName(".item__img");
+img.appendChild;
+console.log(img);
+
+document.getElementById("title").textContent = "Je suis le nom du...";
+
+document.getElementById("price").textContent = "5555";
+
+document.getElementById("description").textContent = "blablablablablaaaaaa";
+
+// Ajout des options de couleurs avec le constructeur Option().
+
+let item_colors = document.getElementById("colors");
+let options = ["Vert", "Bleu", "Jaune"];
+
+options.forEach(function (element, key) {
+  if (element == "Vert") {
+    item_colors[item_colors.options.length] = new Option(
+      element,
+      item_colors.options.length,
+      false,
+      false
+    );
+  }
+  if (element == "Bleu") {
+    item_colors[item_colors.options.length] = new Option(
+      element,
+      item_colors.options.length,
+      true,
+      false
+    ); // Ajouter l'attribut "selected"
+  }
+  if (element == "Jaune") {
+    item_colors[item_colors.options.length] = new Option(
+      element,
+      item_colors.options.length,
+      true,
+      false
+    ); // Sélectionnera l'option
+  }
+});
+
+/*
 
 let items = document.getElementsByClassName("item");
 
@@ -27,3 +73,4 @@ item_description.textContent = newUrlSearchParams.description;
 item_colors = newUrlSearchParams.colors;
 
 console.log(item_description);
+*/
