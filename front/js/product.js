41d480d8ade4f32de7d032dd9f142fd661eec652
nicolasquirin <nicolasquirin22@gmail.com>
2022-02-15 16:40:49 +0100

    update-add-Basket_product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,9 +1,9 @@
 // Récupération de la chaine de requête Url de la page actuel (methode window.location).
 
 const queryString_url_id = window.location.search;
-
+//
 // Récupération de l'Id du produit selectionner avec la méthode UrlSearchParams.
-
+//
 const urlSearcheParams = new URLSearchParams(queryString_url_id);
 const newUrlSearchParams = urlSearcheParams.get("id");
 console.log(newUrlSearchParams);
@@ -15,17 +15,18 @@ const fetchDatas = async () => {
     .then((res) => res.json())
     .then(
       (promise = (datas = promise) => {
+        //
         // Inplantation des elements dans le DOM.
-
+        //
         const img = document.createElement("img");
         (img.src = datas.imageUrl), (img.alt = datas.altTxt);
         document.querySelector(".item__img").appendChild(img);
         document.getElementById("title").textContent = datas.name;
         document.getElementById("price").textContent = datas.price;
         document.getElementById("description").textContent = datas.description;
-
+        //
         // Ajout des options de couleurs avec le constructeur Option().
-
+        //
         let optionColors = datas.colors;
         let item_colors = document.getElementById("colors");
         let options = [
@@ -48,5 +49,35 @@ const fetchDatas = async () => {
         console.log(optionColors);
       })
     );
+  addBasket(datas);
 };
+//
+// Initialisation de l'affichage des donées API du produit selectionné(ID) pour product.html
+//
 fetchDatas();
+
+//
+// Ajout du produit
+//
+
+const addBasket = () => {
+  let clickButton = document.getElementById("addToCart");
+  clickButton.addEventListener("click", () => {
+    let optionColors = document.getElementById("colors");
+    let quantityProduct = document.getElementById("quantity");
+    let productStorage = JSON.parse(localStorage.getItem("productStorage"));
+
+    const valueColor = Object.assign({}, datas, {
+      item_colors: `${optionColors.value}`,
+      quantity: 1,
+    });
+
+    console.log(valueColor);
+
+    if (productStorage == null) {
+      productStorage = [];
+      productStorage.push(valueColor);
+      localStorage.setItem("productStorage", JSON.stringify(productStorage));
+    }
+  });
+};
